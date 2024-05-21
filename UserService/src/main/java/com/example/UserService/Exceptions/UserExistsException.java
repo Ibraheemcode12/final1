c5d63007db34bd2f3907292c2262baf659b848dc
70IQ <106028267+Ibraheemcode12@users.noreply.github.com>
2024-05-21 09:37:14 +0300

    Delete UserService directory

--- UserService/src/main/java/com/example/UserService/Exceptions/UserExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.example.UserService.Exceptions;
-
-public class UserExistsException extends Exception{
-
-    public UserExistsException(String errmsg){
-        super(errmsg);
-    }
-    
-}
